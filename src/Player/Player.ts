c3ba7df4dd82361a673b9e09744fe6f5edbd25ce
Alina Nosovets <alinanosovets6849@gmail.com>
2024-02-17 21:03:13 +0700

    feat: create Player

---
+++ src/Player/Player.ts
@@ -0,0 +1,16 @@
+import { WebSocket } from 'ws';
+
+export class Player {
+  name: string;
+  password: string;
+  index: number;
+  ws: WebSocket;
+
+  constructor(name: string, password: string, ws: WebSocket, index: number) {
+    this.password = password;
+    this.name = name;
+    this.index = index;
+    this.ws = ws; 
+  }
+
+}
